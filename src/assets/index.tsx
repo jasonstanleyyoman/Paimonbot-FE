a0a6cf79a773bc56f34d07282fa06111d6a0e04c
Jason Stanley Yoman <jasonstanleyyoman@gmail.com>
2021-04-27 23:14:38 +0700

    add asset

---
+++ src/assets/index.tsx
@@ -0,0 +1,13 @@
+import Hutao from "./hutao-removebg-preview.png"
+import DilucNote from "./noted.png"
+import ZilongQuestion from "./question.png"
+import DilucSent from "./sent.png"
+import KeqingStudy from "./study-removebg-preview.png"
+
+export {
+    Hutao,
+    DilucNote,
+    ZilongQuestion,
+    DilucSent,
+    KeqingStudy
+}
