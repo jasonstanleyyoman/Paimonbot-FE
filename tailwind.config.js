732aaf68f57ae8d07dbae51fee3736bc72937966
Jason Stanley Yoman <jasonstanleyyoman@gmail.com>
2021-04-27 17:41:04 +0700

    add scrollbar and custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            keyframes: {
+                customBounce: {
+                    "0%, 100%": {
+                        transform: `translateY(-75%)`,
+                        animationTimingFunction: `cubic-bezier(0.8, 0, 1, 1)`,
+                    },
+                    "50%": {
+                        transform: `translateY(0)`,
+                        animationTimingFunction: `cubic-bezier(0, 0, 0.2, 1)`,
+                    },
+                },
+            },
+            animation: {
+                customBounce: "customBounce 1s infinite",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require("tailwind-scrollbar")],
+};
